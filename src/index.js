89f019f575c4ebf8d877d614b61de5aea66ccaa6
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2021-08-17 23:02:35 +0300

    Adds homework solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './sass/style.scss';
+
+import menu from "./menu.json";
+import cardsTpl from "./templates/cards.hbs";
+
+
+const menuList = document.querySelector('.js-menu');
+const body = document.querySelector("body");
+const checkbox = document.querySelector('#theme-switch-toggle');
+
+const menuMarkup = cardsTpl(menu);
+menuList.insertAdjacentHTML('beforeend', menuMarkup);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const onChangeTheme = () => {
+
+    if (checkbox.checked) {
+        body.classList.add(Theme.DARK);
+        body.classList.remove(Theme.LIGHT);
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        body.classList.add(Theme.LIGHT);
+        body.classList.remove(Theme.DARK);
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+};
+
+const onCheckboxChecked = () => {
+
+    if (localStorage.getItem('theme') === Theme.DARK) {
+        body.classList.add(Theme.DARK);
+        checkbox.checked = true;
+    }
+};
+
+checkbox.addEventListener('change', onChangeTheme);
+onCheckboxChecked();
